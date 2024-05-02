d7fcd0c1f2d61906e2bafec52f9f6ce59a9f10fc
Svyatoslav Krivosheev <t1ksy@mail.ru>
2024-05-03 02:31:35 +0300

    Сформировал схему проекта. Сделал TODO лист на завтра

--- DjangoProject24/blogapp/urls.py
+++ DjangoProject24/blogapp/urls.py
@@ -1,12 +1,28 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+
+
 urlpatterns = [
+    path('articles/', views.view_all_articles, name='all_articles'),  # Страница со списком всех статей
+    path('authors/', views.view_all_authors, name='all_authors'),  # Страница со списком всех авторов
+    path('comments/', views.view_all_comments, name='all_comments'),  # Страница со списком всех комментариев
+    path('author/add/', views.add_author_form, name='add_author'),  # Страница для добавления нового автора
     path('write_authors/', views.write_authors, name='write_authors'),
     path('about/', views.about, name='about'),
     path('about/me/', views.aboutme, name='aboutme'),
-    path('author/add/', views.add_author_form, name='add_author_form'),
-    path('author/all', views.view_all_authors, name='view_all_authors'),
-    path('article/all', views.view_all_articles, name='view_all_articles'),
-    path('comment/all', views.view_all_comments, name='view_all_comments'),
 ]
+
+# TODO list
+# path('author/<int:author_id>/articles/', views.author_articles, name='author_articles'), # Страница со списком статей конкретного автора
+# path('article/add/', views.add_article_form, name='add_article'), # Страница для добавления новой статьи
+# path('article/<int:article_id>/comment/', views.add_comment_form, name='add_comment'), # Страница для добавления нового комментария
+# path('article/<int:article_id>/', views.article_detail, name='article_detail'), # Страница с деталями конкретной статьи
+# path('author/<int:author_id>/', views.author_detail, name='author_detail'), # Страница с деталями конкретного автора
+# path('article/<int:article_id>/edit/', views.edit_article_form, name='edit_article'), # Страница с формой для редактирования статьи
+# path('author/<int:author_id>/edit/', views.edit_author_form, name='edit_author'), # Страница с формой для редактирования автора
+# path('comment/<int:comment_id>/edit/', views.edit_comment_form, name='edit_comment'), # Страница с формой для редактирования комментария
+# path('article/<int:article_id>/delete/', views.delete_article_confirm, name='delete_article'), # Страница с формой для удаления статьи
+# path('author/<int:author_id>/delete/', views.delete_author_confirm, name='delete_author'), # Страница с формой для удаления автора
+# path('comment/<int:comment_id>/delete/', views.delete_comment_confirm, name='delete_comment'), # Страница с формой для удаления комментария
+#
