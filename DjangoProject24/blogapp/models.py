6b50800eed75ef9d1f27f95702989af363b35eb1
Svyatoslav Krivosheev <t1ksy@mail.ru>
2024-05-02 22:28:08 +0300

    Добавил модель статья и дополнил модель автор (добавил свойство "полное имя")
    Создал форму для добавления новго автора, добавил к ней представление, прописал маршрут, создал шаблон.

--- DjangoProject24/blogapp/models.py
+++ DjangoProject24/blogapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Author(models.Model):
@@ -13,3 +14,20 @@ class Author(models.Model):
                 f'Last name: {self.last_name},\n'
                 f'Email: {self.email},\n'
                 f'Birthday: {self.birthday}')
+
+    @property
+    def full_name(self):
+        return f'{self.first_name} {self.last_name}'
+
+
+class Article(models.Model):
+    author = models.ForeignKey('Author', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    published_date = models.DateTimeField(default=timezone.now)
+    category = models.CharField(max_length=100)
+    views = models.IntegerField(default=0)
+    published = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
