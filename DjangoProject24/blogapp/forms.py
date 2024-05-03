3d835bd34c4412309aef1595bd3e346ef67950af
Svyatoslav Krivosheev <t1ksy@mail.ru>
2024-05-03 17:21:23 +0300

    Создал формы для добавления новой статьи и нового комментария, форму редактирования статьи и автора, подробности статьи и автора.

--- DjangoProject24/blogapp/forms.py
+++ DjangoProject24/blogapp/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Author
+from .models import Author, Article, Comment
 
 
 class AuthorForm(forms.ModelForm):
@@ -13,3 +13,17 @@ class AuthorForm(forms.ModelForm):
             'biography': forms.Textarea(attrs={'class': 'form-control', 'rows': 5, 'cols': 40}),
             'birthday': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
         }
+
+
+class ArticleForm(forms.ModelForm):
+    class Meta:
+        model = Article
+        fields = ['author', 'title', 'content', 'published_date', 'category', 'views', 'published']
+        widgets = {}
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['author', 'content']
+        widgets = {}
